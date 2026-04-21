ee10b24e7afa53bf928b81b0e036cfeb20ba283e
Anna <blinova.anneta@gmail.com>
2026-04-21 17:36:46 +0200

    feat: add testing reset endpoint

--- src/db/in-memory.db.ts
+++ src/db/in-memory.db.ts
@@ -15,22 +15,5 @@ export const db: DB = {
       websiteUrl: "https://node-notes.dev",
     },
   ],
-  posts: [
-    {
-      id: "1",
-      title: "Getting Started with React",
-      shortDescription: "React basics for beginners.",
-      content: "Long post text...",
-      blogId: "1",
-      blogName: "Frontend Journey",
-    },
-    {
-      id: "2",
-      title: "Understanding Express Routing",
-      shortDescription: "How routing works in Express.",
-      content: "Long post text...",
-      blogId: "2",
-      blogName: "Node Notes",
-    },
-  ],
+  posts: [],
 }
--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,7 +1,9 @@
 import express, { Express } from "express";
 import {blogsRouter} from "./blogs/routers/blogs.router";
-import {BLOGS_PATH, POSTS_PATH} from "./core/path/path";
+import {BLOGS_PATH, POSTS_PATH, TESTING_PATH} from "./core/path/path";
 import {postsRouter} from "./posts/routers/posts.router";
+import {setupSwagger} from "./core/swagger/setup-swagger";
+import {testingRouter} from "./testing/routers/testing.router";
 
 export const setupApp = (app: Express) => {
   app.use(express.json()); // middleware для парсинга JSON в теле запроса
@@ -14,6 +16,9 @@ export const setupApp = (app: Express) => {
   // Подключаем роутеры
   app.use(BLOGS_PATH, blogsRouter);
   app.use(POSTS_PATH, postsRouter);
+  app.use(TESTING_PATH, testingRouter);
+
+  setupSwagger(app);
 
   return app;
 };
