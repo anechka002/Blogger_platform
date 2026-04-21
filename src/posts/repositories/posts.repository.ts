03c97e47189c54e31094d012d1bfcfb3fb246027
Anna <blinova.anneta@gmail.com>
2026-04-21 13:31:17 +0200

    docs: fix posts repository comments

--- src/posts/repositories/posts.repository.ts
+++ src/posts/repositories/posts.repository.ts
@@ -3,23 +3,23 @@ import {Post} from "../types/post";
 import {UpdatePostDto} from "../dto/updatePostDto";
 
 export const postsRepository = {
-  // Найти все блоги
+  // Найти все посты
   findAll(): Post[] {
     return db.posts;
   },
 
-  // Найти блог по ID
+  // Найти пост по ID
   findById(id: string): Post | null {
     return db.posts.find((b) => b.id === id) ?? null;
   },
 
-  // Создать новый блог
+  // Создать новый пост
   create(post: Post): Post {
     db.posts.push(post);
     return post;
   },
 
-  // Обновить данные бдога
+  // Обновить данные поста
   update(id: string, dto: UpdatePostDto, blogName: string): void {
     const post = db.posts.find((b) => b.id === id);
     if (!post) return;
@@ -33,7 +33,7 @@ export const postsRepository = {
     return
   },
 
-  // Удалить блог
+  // Удалить пост
   delete(id: string): void {
     const index = db.posts.findIndex((b) => b.id === id);
     if (index === -1) return;
