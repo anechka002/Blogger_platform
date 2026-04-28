e83dfa592e25c491dab849c88900c48ae938a010
Anna <blinova.anneta@gmail.com>
2026-04-28 11:39:05 +0200

    fix env

--- src/core/settings/settings.ts
+++ src/core/settings/settings.ts
@@ -1,5 +1,9 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 export const SETTINGS =  {
   PORT: process.env.PORT || 5001,
-  MONGO_URL: process.env.MONGO_URL || 'mongodb+srv://root:root@clustermongodb.98xltqo.mongodb.net/?appName=ClusterMongoDB',
+  MONGO_URL: process.env.MONGO_URL || 'mongodb://0.0.0.0:27017',
   DB_NAME: process.env.DB_NAME || 'blogger_platform',
 }
