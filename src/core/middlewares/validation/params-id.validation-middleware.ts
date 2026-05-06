c122a285ff025160c3e76438be811a088859e553
Anna <blinova.anneta@gmail.com>
2026-05-06 19:52:48 +0200

    fix id validation Middleware

--- src/core/middlewares/validation/params-id.validation-middleware.ts
+++ src/core/middlewares/validation/params-id.validation-middleware.ts
@@ -1,7 +1,9 @@
 import {param} from "express-validator";
 
-export const idValidationMiddleware = param('id')
-  .notEmpty().withMessage("ID is required") // Проверка, что строка не пустая
-  .isString().withMessage('ID must be a string') // Проверка, что это строка
-  .isMongoId()
-  .withMessage('Incorrect format of ObjectId'); // Проверка на формат ObjectId
+export const idValidationMiddleware = (paramName: string = 'id') => {
+  return param(paramName)
+    .notEmpty().withMessage(`${paramName} is required`) // Проверка, что строка не пустая
+    .isString().withMessage(`${paramName} must be a string`) // Проверка, что это строка
+    .isMongoId()
+    .withMessage('Incorrect format of ObjectId'); // Проверка на формат ObjectId
+}
