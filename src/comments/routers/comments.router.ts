9979545046c1dcd83c9c1541911088ce33037b0d
Anna <blinova.anneta@gmail.com>
2026-05-27 11:10:43 +0200

    Add get comment endpoint

--- src/comments/routers/comments.router.ts
+++ src/comments/routers/comments.router.ts
@@ -1,5 +1,13 @@
 import {Router} from "express";
+import {getCommentHandler} from "./handlers/get-comment.handler";
+import {
+  idValidationMiddleware
+} from "../../core/middlewares/validation/params-id.validation-middleware";
+import {
+  inputValidationResultMiddleware
+} from "../../core/middlewares/validation/input-validation-result.middleware";
 
 export const commentsRouter = Router({});
 
 commentsRouter
+  .get('/:id', idValidationMiddleware(), inputValidationResultMiddleware, getCommentHandler)
