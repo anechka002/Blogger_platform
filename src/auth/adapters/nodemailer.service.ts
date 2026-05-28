552a53075f359726e0739ecc0891289d4b54373f
Anna <blinova.anneta@gmail.com>
2026-05-28 15:50:46 +0200

    add nodemailer

---
+++ src/auth/adapters/nodemailer.service.ts
@@ -0,0 +1,23 @@
+import nodemailer from "nodemailer";
+import {SETTINGS} from "../../core/settings/settings";
+
+export const nodemailerService = {
+  async sendEmail(email: string, subject: string, message: string): Promise<boolean> {
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: SETTINGS.EMAIL,
+        pass: SETTINGS.EMAIL_PASSWORD,
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: '"Anna 👻" <SETTINGS.EMAIL>', // от кого: Gmail
+      to:  email, // кому: Mail.ru
+      subject: subject,
+      html: message,
+    });
+
+    return !!info
+  }
+}
